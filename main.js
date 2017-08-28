ecc39e08a682492b6330805e964789dd77891433
anellla <anellla89@mail.ru>
2017-08-28 05:06:53 +0300

    Finished Task

--- main.js
+++ main.js
@@ -1,48 +1,67 @@
 'use strict';
 
 const metasync = require('metasync');
-
-//console.log('We use metasync:');
-//console.log(Object.keys(metasync));
 const concolor = require('concolor');
-//console.log('CONCOLOR methods: ');
-//console.log(Object.keys(concolor));
-// let a = Object.keys(metasync);
-const a = Object.keys(metasync);
 
-for (let i = 0, m = a.length;  i < m;  i++) {
-  const b = Object.keys(metasync)[i];
-  console.log( 'Метод ' + (parseInt(i) + 1) + ' : ' + '' + concolor`${b}(blue/red)`);
+// console.log('We use metasync:');
+// console.log(Object.keys(metasync));
+// console.log('CONCOLOR methods: ');
+// console.log(Object.keys(concolor));
+
+
+/*Variant 1 - with simple for*/
+console.log(concolor`${'Способ 1'}(b,u,red)`);
+for (let i = 0, m = Object.keys(metasync).length; i < m; i++) {
+  if ((parseInt(i) + 1) % 2 === 0) {
+    console.log(concolor`${'Метод ' + (parseInt(i) + 1)}(b,/blue)` + ' : ' + concolor`${Object.keys(metasync)[i]}(b,u,yellow)`);
+  }
+  else {
+    console.log(concolor`${'Метод ' + (parseInt(i) + 1)}(b,/green)` + ' : ' + concolor`${Object.keys(metasync)[i]}(b,u,blue)`);
+  }
 }
+/*Конец варианта 1*/
 
-//onsole.log(concolor`Hello ${'World'}(blue,u) blue underline`);
+console.log();
 
-console.log(concolor`Hello ${'World'}(blue) blue`);
-console.log(concolor`Hello ${'World'}(/red) on red`);
-console.log(concolor`Hello ${'World'}(white/yellow,b) bold white on yellow`);
-console.log(concolor`Hello ${'World'}(b) bold`);
-console.log(concolor`Hello ${'World'}(b,/blue) bold on blue`);
-console.log(concolor`Hello ${'World'}(b,u,yellow) bold underline yellow`);
-console.log(concolor`Hello ${'World'}(blue,u) blue underline`);
-console.log(concolor`Hello ${'World'}(b,black/green) bold black on green`);
- 
-console.log(concolor`
-  Client on ${'192.168.1.1'}(black/green)
-  connected to ${'SERVER'}(b,red)
-  at ${new Date().toUTCString()}(b,blue)
-`);
+/*Вариант 2*/
+const title = concolor('b,u,  /red');
+const name = concolor('b,yellow');
+const method = concolor('b,blue');
+const name2 = concolor('b,blue');
+const method2 = concolor('b,yellow');
 
-const err = concolor('b,yellow/red');
-const inf = concolor('i,white');
- 
+console.log(title('Способ 2'));
+for (const i in Object.keys(metasync)) {
+    if ((parseInt(i) + 1) % 2 !== 0) {
+  console.log(name2('Метод ' + (parseInt(i) + 1)) + ' : ' + method2(Object.keys(metasync)[i]));
+  }
+    else {
+  console.log(name('Метод ' + (parseInt(i) + 1)) + ' : ' + method(Object.keys(metasync)[i]));
+  }
+}
+/*Конец варианта 2*/
 
+console.log();
 
-
-
+/*Variant 3 - with theme and for...of)*/
 const theme = concolor({
-  caption: 'b,white',
-  text: 'green',
-  link: 'u,yellow'
+  text: 'b,white',
+  method1: 'green',
+  number: 'blue'
 });
- 
-console.log(theme.caption('Caption example'));
+console.log('Способ 3');
+let i = 0;
+for (const method of Object.keys(metasync)) {
+  i++;
+  console.log(theme.text('Метод ' + i) + theme.method1(' :  ' + method));
+}
+/*End of variant 3 - with theme and for...of)*/
+
+console.log();
+/*Variant 4*/
+//arrow-callback
+console.log('Способ 4');
+Object.keys(metasync).forEach(function(method, number) {
+  console.log('Метод ' + (parseInt(number) + 1) + ' : ' + concolor`${method}(red)`);
+});
+/*End of variant 4*/
